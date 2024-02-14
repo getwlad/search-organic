712e572e4f4bc3b798a873917831f9a2ebc05c9d
getwlad <wladmir.rrodrigues@gmail.com>
2024-02-14 13:42:51 -0300

    fksoksd

--- src/main/java/com/vemser/dbc/searchorganic/security/SecurityConfiguration.java
+++ src/main/java/com/vemser/dbc/searchorganic/security/SecurityConfiguration.java
@@ -93,7 +93,7 @@ public class SecurityConfiguration {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("https://search-organic.vercel.app/")
+                        .allowedOrigins("https://search-organic.vercel.app/", "https://search-organic-production.up.railway.app/")
                         .allowedMethods("*")
                         .exposedHeaders("Authorization");
             }
