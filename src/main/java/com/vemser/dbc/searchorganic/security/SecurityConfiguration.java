ddeb02a1b69c8f3ccfbbc801d6701a56cd08f2d1
getwlad <wladmir.rrodrigues@gmail.com>
2024-02-16 13:38:41 -0300

    fix: ajuste deploy sem port

--- src/main/java/com/vemser/dbc/searchorganic/security/SecurityConfiguration.java
+++ src/main/java/com/vemser/dbc/searchorganic/security/SecurityConfiguration.java
@@ -93,7 +93,7 @@ public class SecurityConfiguration {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://vemser-dbc.dbccompany.com.br:38080", "https://search-organic.vercel.app")
+                        .allowedOrigins("http://vemser-dbc.dbccompany.com.br", "https://search-organic.vercel.app")
                         .allowedMethods("*")
                         .exposedHeaders("Authorization");
             }
