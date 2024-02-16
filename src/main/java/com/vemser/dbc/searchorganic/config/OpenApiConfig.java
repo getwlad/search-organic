0642ff9def52cc0637c39e3a4866f4213097b030
getwlad <wladmir.rrodrigues@gmail.com>
2024-02-16 13:30:55 -0300

    b

--- src/main/java/com/vemser/dbc/searchorganic/config/OpenApiConfig.java
+++ src/main/java/com/vemser/dbc/searchorganic/config/OpenApiConfig.java
@@ -6,11 +6,8 @@ import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
-import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import java.util.List;
 
 @Configuration
 public class OpenApiConfig {
@@ -33,7 +30,6 @@ public class OpenApiConfig {
                                                 .scheme("bearer")
                                                 .bearerFormat("JWT")
                                 )
-
-               );
+                );
     }
 }
